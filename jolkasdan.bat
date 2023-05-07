3c7ea34f7875f34b1eff4100eb1b6992c44e626a
KENZOBASAR <132684139+KENZOBASAR@users.noreply.github.com>
2023-05-07 05:41:55 -0700

    its an update
    alpha v0.0.1

---
+++ jolkasdan.bat
@@ -0,0 +1,2 @@
+java -Xmx1024M -Xms1024M -cp main code.folder "-Dorg.lwjgl.librarypath=%CD%/main code.folder" "-Dnet.java.games.input.librarypath=%CD%/jdk-8u191-windows-i586.exe" Start
+pause
---
+++ readme!.txt
@@ -0,0 +1,5 @@
+follow me on youutube   @j8gho814                     verision : alpha v0.0.1
+coded with java 
+
+windows 10+ btw i maked it with windows 10 
+and i dont even test it
